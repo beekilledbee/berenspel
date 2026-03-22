4dae678996952aa97deba8b1437c4183e267d094
Jacob Choi <jongwoochoi09@gmail.com>
2026-03-22 02:24:39 +0100

    tilemap class + relative path for tsx

---
+++ tilemap.py
@@ -0,0 +1,19 @@
+import pygame
+import pytmx
+
+
+class TileMap:
+    def __init__(self, filename: str):
+        self.tmx_data = pytmx.load_pygame(filename)
+        self.tile_width = self.tmx_data.tilewidth
+        self.tile_height = self.tmx_data.tileheight
+        self.width = self.tmx_data.width * self.tile_width
+        self.height = self.tmx_data.height * self.tile_height
+
+    def draw(self, surface: pygame.Surface) -> None:
+        for layer in self.tmx_data.visible_layers:
+            if isinstance(layer, pytmx.TiledTileLayer):
+                for x, y, gid in layer:
+                    tile = self.tmx_data.get_tile_image_by_gid(gid)
+                    if tile:
+                        surface.blit(tile, (x * self.tile_width, y * self.tile_height))
